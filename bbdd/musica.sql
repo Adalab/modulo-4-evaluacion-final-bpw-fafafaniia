805cf6cd04cdba17ef85856b5667f8a7fc63ce97
Estefanía <fafafaniia95@gmail.com>
2025-03-17 18:50:48 +0100

    I can't allow it to be erased

--- bbdd/musica.sql
+++ bbdd/musica.sql
@@ -33,7 +33,7 @@ CREATE TABLE `albums` (
   PRIMARY KEY (`album_id`),
   KEY `fk_albums_artists_idx` (`artist_id`),
   CONSTRAINT `fk_albums_artists` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`artist_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +42,7 @@ CREATE TABLE `albums` (
 
 LOCK TABLES `albums` WRITE;
 /*!40000 ALTER TABLE `albums` DISABLE KEYS */;
+INSERT INTO `albums` VALUES (1,'HIT ME HARD AND SOFT','2024-05-17','https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62',1);
 /*!40000 ALTER TABLE `albums` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,8 +58,10 @@ CREATE TABLE `artists` (
   `name` varchar(255) NOT NULL,
   `birthdate` date NOT NULL,
   `nationality` varchar(45) NOT NULL,
+  `biography` text,
+  `picture` text,
   PRIMARY KEY (`artist_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +70,7 @@ CREATE TABLE `artists` (
 
 LOCK TABLES `artists` WRITE;
 /*!40000 ALTER TABLE `artists` DISABLE KEYS */;
+INSERT INTO `artists` VALUES (1,'Billie Eilish','2001-12-18','Estadounidense','Billie Eilish Pirate Baird O.Connell (nacida el 18 de diciembre de 2001 en Los Ángeles, California) es una cantante y compositora estadounidense que se hizo mundialmente conocida por su estilo único y su música ecléctica que abarca géneros como el pop, el electropop y el indie. Empezó a componer canciones a una edad temprana y, con solo 14 años, publicó su sencillo \"Ocean Eyes\" en 2015, que rápidamente ganó popularidad en plataformas como SoundCloud.\nEn 2019, Eilish lanzó su álbum debut, \"When We All Fall Asleep, Where Do We Go?\", que fue un éxito crítico y comercial, catapultándola a la fama global. Su estilo musical oscuro y su estética visual, junto con sus letras introspectivas, la hicieron destacar en la industria de la música. Billie Eilish ha ganado múltiples premios, incluidos varios Grammy en 2020, donde se llevó el premio a Álbum del Año, Canción del Año y Mejor Nuevo Artista, entre otros.\nA lo largo de su carrera, Eilish ha sido reconocida por su autenticidad, su imagen única y su influencia en la cultura pop moderna. Además de su música, también se ha destacado por sus posturas sobre la salud mental, el cuerpo y la imagen, y su enfoque en la autoaceptación.','https://www.totalisimo.com/wp-content/uploads/2024/08/billie-eilish-sconvierte-artista-mas-escuchada-mundo.jpeg');
 /*!40000 ALTER TABLE `artists` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,8 +84,9 @@ DROP TABLE IF EXISTS `genres`;
 CREATE TABLE `genres` (
   `genre_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
+  `description` text,
   PRIMARY KEY (`genre_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,6 +95,7 @@ CREATE TABLE `genres` (
 
 LOCK TABLES `genres` WRITE;
 /*!40000 ALTER TABLE `genres` DISABLE KEYS */;
+INSERT INTO `genres` VALUES (1,'Bedroom pop','El Bedroom Pop es un subgénero del indie pop que se caracteriza por su producción lo-fi, a menudo grabada en un dormitorio o estudio casero . Se caracteriza por su sonido meloso y soñador, a menudo con guitarras acústicas, sintetizadores y voces suaves.'),(2,'Pop','Debe su nombre a la abreviación “popular music” que, como bien nos indica en inglés, hace referencia a los sonidos, temas y melodías para el público de masas. Las composiciones sencillas y los ritmos ligeros son algunas de las principales características más representativas del pop.'),(3,'Tech-house','El término tech house se desarrolló como un nombre abreviado en las tiendas de discos para una categoría de música electrónica de baile que combinaba aspectos musicales del techno, como \"líneas de bajo robustas\" y \"ritmos acerados\", con las armonías y los ritmos del house progresivo .'),(4,'Dream pop','El dream pop es un subgénero de la música pop que se desarrolló en la década de 1980. Se caracteriza por su sonido suave y etéreo, con influencias de la música electrónica y el rock psicodélico.'),(5,'Progressive R&B','El soul progresivo (a menudo abreviado como prog-soul; también llamado prog negro, rock negro y R&B progresivo) es un tipo de música afroamericana que utiliza un enfoque progresivo, particularmente en el contexto de los géneros soul y funk .'),(6,'Tech-house','El término tech house se desarrolló como un nombre abreviado en las tiendas de discos para una categoría de música electrónica de baile que combinaba aspectos musicales del techno, como \"líneas de bajo robustas\" y \"ritmos acerados\", con las armonías y los ritmos del house progresivo .');
 /*!40000 ALTER TABLE `genres` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,6 +123,7 @@ CREATE TABLE `genres_has_songs` (
 
 LOCK TABLES `genres_has_songs` WRITE;
 /*!40000 ALTER TABLE `genres_has_songs` DISABLE KEYS */;
+INSERT INTO `genres_has_songs` VALUES (1,2),(2,2),(4,2),(3,4),(5,4);
 /*!40000 ALTER TABLE `genres_has_songs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,14 +190,13 @@ DROP TABLE IF EXISTS `songs`;
 CREATE TABLE `songs` (
   `song_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `legth` time DEFAULT NULL,
-  `genre` varchar(45) DEFAULT NULL,
+  `length` time DEFAULT NULL,
   `embed_code` text,
   `albums_album_id` int NOT NULL,
   PRIMARY KEY (`song_id`),
   KEY `fk_songs_albums1_idx` (`albums_album_id`),
   CONSTRAINT `fk_songs_albums1` FOREIGN KEY (`albums_album_id`) REFERENCES `albums` (`album_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -199,6 +205,7 @@ CREATE TABLE `songs` (
 
 LOCK TABLES `songs` WRITE;
 /*!40000 ALTER TABLE `songs` DISABLE KEYS */;
+INSERT INTO `songs` VALUES (2,'SKINNY','00:03:39','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/1CsMKhwEmNnmvHUuO5nryA?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(3,'LUNCH','00:02:59','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/629DixmZGHc7ILtEntuiWE?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(4,'CHIHIRO','00:05:03','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/7BRD7x5pt8Lqa1eGYC4dzj?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(5,'BIRDS OF A FEATHER','00:03:30','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/6dOtVTDdiauQNBQEDOtlAB?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(6,'WILDFLOWER','00:04:21','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/3QaPy1KgI7nu9FJEQUgn6h?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(7,'THE GREATEST','00:04:53','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/6TGd66r0nlPaYm3KIoI7ET?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(8,'L\'AMOUR DE MA VIE','00:05:33','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/6fPan2saHdFaIHuTSatORv?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(9,'THE DINER','00:03:06','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/1LLUoftvmTjVNBHZoQyveF?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(10,'BITTERSUITE','00:04:58','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/7DpUoxGSdlDHfqCYj0otzU?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1),(11,'BLUE','00:05:43','<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/track/2prqm9sPLj10B4Wg0wE5x9?utm_source=generator\" width=\"100%\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>',1);
 /*!40000 ALTER TABLE `songs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -256,14 +263,6 @@ LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'musica'
---
-
---
--- Dumping routines for database 'musica'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -274,4 +273,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-16 12:32:46
+-- Dump completed on 2025-03-16 22:37:20
--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const mysql = require("mysql/promise");
+const mysql = require("mysql2/promise");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 async function getConnection() {
     const connectionData ={
-        host: process.env["MySQL_HOST"],
+        host: process.env["MySQL_HOST"], 
         port: process.env["MySQL_PORT"],
         user: process.env["MySQL_USER"],
         password: process.env["MySQL_PASS"],
         database: process.env["MySQL_SCHEMA"]
     };
-
+     
     const connection = await mysql.createConnection(connectionData);
     await connection.connect();
 
     return connection;
 };
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 const port = 3000;
 app.listen(port, () => {
@@ -30,4 +31,21 @@ app.listen(port, () => {
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
-  });
+});
+
+app.get('/songs', async (req,res) => {
+    const conn = await getConnection();
+
+    const [result] = await conn.query(
+        `SELECT * 
+         FROM songs`
+    ); 
+
+    await conn.end();
+
+    res.send(result)
+} );
+
+
+
+
